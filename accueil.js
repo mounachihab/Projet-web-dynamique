2d71982b3402ebee33006db53519087fb56a9898
Elizbrn <148560661+Elizbrn@users.noreply.github.com>
2023-12-06 21:46:37 +0100

    Add files via upload

---
+++ accueil.js
@@ -0,0 +1,9 @@
+function afficherMessageIntro() {
+    // Afficher la boîte de dialogue
+    document.getElementById("overlayContainer").style.display = "flex";
+}
+
+function cacherMessageIntro() {
+    // Cacher la boîte de dialogue
+    document.getElementById("overlayContainer").style.display = "none";
+}
