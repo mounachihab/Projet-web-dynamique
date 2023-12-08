6c4711835c328281a28b2c6ada5966b73629995a
Elizbrn <148560661+Elizbrn@users.noreply.github.com>
2023-12-09 00:07:38 +0100

    Add files via upload

--- accueil.js
+++ accueil.js
@@ -1,14 +1,14 @@
 
 
 // Pour l'intro dans le logo
-function afficherMessageIntro() {
+function afficher_message_intro() {
     // Afficher la boîte de dialogue
-    document.getElementById("overlayContainer").style.display = "flex";
+    document.getElementById("container").style.display = "flex";
 }
 
-function cacherMessageIntro() {
+function cacher_message_intro() {
     // Cacher la boîte de dialogue
-    document.getElementById("overlayContainer").style.display = "none";
+    document.getElementById("container").style.display = "none";
 }
 
 
@@ -67,3 +67,32 @@ $(document).ready(function(){
     maBoucle(); // lance la fonction une première fois
 
 });
+
+
+// Fonction pour remonter au début de la page
+function retour_en_haut() {
+    document.documentElement.scrollTop = 0; //remonte vers le haut, en 0
+}
+
+
+
+
+// fonction pour afficher en decembre
+// Fonction pour afficher l'image en fonction de la date
+function affiche_image_date() {
+    //  On regarde la date actuelle
+    var dateActuelle = new Date();
+
+    // Vérifier si c'est decembre (va de 0 à 11)
+    if (dateActuelle.getMonth() === 11) {
+        // Si le mois est décembre, afficher l'image
+        document.getElementById('image_dec_1').src = "dec/dec_sapins.png";
+        document.getElementById('image_dec_2').src = "dec/dec_sapin.png";
+    } else {
+        // Sinon on n'affiche pas
+        document.getElementById('image_dec').src = '';
+    }
+}
+
+// Appeler la fonction au chargement de la page
+window.onload = affiche_image_date;
