b4273d7fc3960c8d45b51794db95f385f10883e2
juliehirtzmann <149301973+juliehirtzmann@users.noreply.github.com>
2023-12-13 18:07:07 +0100

    Create messagerie.php

---
+++ messagerie.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="fr">
+<!-- -->
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="messagerie.css">
+    <form action="monreseau.php" method="post">
+    <title>ECE In - Ma messagerie </title>
+</head>
+
+<body>
+    <!-- Partie du logo -->
+    <div id="banderole">
+        <div id="logo">
+            <img src="logo.png"
+                 alt="Logo"
+                 width="260"
+                 height=""/>
+        </div>
+
+        <div id="titre">
+            <h2>ECE In - Messagerie: Discutez avec tous vos amis de l'ECE! </h2>
+        </div>
+
+        <!-- Partie du boutons de deconnexion -->
+        <div id="deco">
+            <img src="bouton_deconnection.png"
+                            alt="Logo"
+                            width="50"
+                            height=""/>
+        </div>
+
+    </div>
+
+    <div id="couleur"></div>
+
+    <div id="wrapper">
+    <!-- Partie des boutons -->
+    <div id="boutons">
+        <img src="bouton_accueil_0.png"
+             alt="accueil.html"
+             width="170"
+             height=""/>
+        <a href="julie.php"> <img src="bouton_mon_reseau_0.png"
+             alt="page reseau"
+             width="170"
+             height=""/><a>
+        <img src="bouton_vous_0.png"
+             alt=".html"
+             width="170"
+             height=""/>
+        <img src="bouton_notification_0.png"
+             alt=".html"
+             width="170"
+             height=""/>
+        <img src="bouton_messagerie_1.png"
+             alt=".html"
+             width="170"
+             height=""/>
+        <img src="bouton_emplois_0.png"
+             alt=".html"
+             width="170"
+             height=""/>
+
+    	</div>
+
+     	<!-- choix affichage -->
+    	<div id="bloc">
+            <div class="scrollable-container">
+             
+             </div>
+    	 
+        </div>
+
+    <!-- Copyright -->
+    <div id="copy">
+        <p>&copy; 2023 ECE In. Tous droits réservés.</p>
+    </div>
+
+</body>
